950e2ac38f46a427dffc6cf10add01b3079b1f68
asib <ahmedasib1@gmail.com>
2024-05-31 17:23:27 +0600

    feat: config url by view mapping

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# view means request -> response
+# request handler
+# action
+
+def say_hello(request):
+    # pull ddata from db
+    # transform
+    # send email
+    return HttpResponse('hello world')
