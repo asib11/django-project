0c8b9c32235de04397eb9e48ddf0651ef63002aa
asib <ahmedasib1@gmail.com>
2024-07-26 23:35:58 +0600

    feat: product model make

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    update_time = models.DateTimeField(auto_now=True)
+
+class Customer
