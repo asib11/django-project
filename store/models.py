631a9e12f4c82cf73b95ce67d9262857e8a94df3
asib <ahmedasib1@gmail.com>
2024-07-27 11:09:23 +0600

    feat: order model make

--- store/models.py
+++ store/models.py
@@ -9,8 +9,29 @@ class Product(models.Model):
     update_time = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICE = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Sliver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=70)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=14)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICE, default=MEMBERSHIP_BRONZE)
+
+class Order(models.Model):
+    PAYMENT_PENDING = 'p'
+    PAYMENT_COMPLETE= 'C'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_PENDING,'Pending'),
+        (PAYMENT_COMPLETE,'Completed'),
+        (PAYMENT_FAILED, 'Failed')
+    ]
+    place_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_PENDING)
